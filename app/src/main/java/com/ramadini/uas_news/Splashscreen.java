17426638c371508d046164850d981ec2c2728f08
fara maulidia <faramaulidia79@gmail.com>
2019-06-06 20:53:35 +0700

    UAS-ANDROID_FARA-DINI

---
+++ app/src/main/java/com/ramadini/uas_news/Splashscreen.java
@@ -0,0 +1,60 @@
+package com.ramadini.uas_news;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Splashscreen extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private TextView persentase;
+    private int Value = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+        progressBar = findViewById(R.id.progress);
+        persentase = findViewById(R.id.persentase);
+        progressBar.setProgress(0); //Set Progress Dimulai Dari O
+
+        // Handler untuk Updating data pada latar belakang
+        final Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                // Menampung semua data yang ingin diproses oleh thread
+                persentase.setText(String.valueOf(Value)+"%");
+                if(Value == progressBar.getMax()){
+                    Toast.makeText(getApplicationContext(), "Progress Completed", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(Splashscreen.this, MainActivity.class));
+                    finish();
+                }
+                Value++;
+            }
+        };
+
+        // Thread untuk updating progress pada ProgressBar di Latar Belakang
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    for(int w=0; w<=progressBar.getMax(); w++){
+                        progressBar.setProgress(w); // Memasukan Value pada ProgressBar
+                        // Mengirim pesan dari handler, untuk diproses didalam thread
+                        handler.sendMessage(handler.obtainMessage());
+                        Thread.sleep(100); // Waktu Pending 100ms/0.1 detik
+                    }
+                }catch(InterruptedException ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+}
